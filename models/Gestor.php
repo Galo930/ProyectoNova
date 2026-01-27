847d4152899377711b06e905dfceb29e568bfd36
Galo930 <aagagu@alumnatflorida.es>
2026-01-27 13:08:57 +0100

    Añadiendo cambios

--- models/Gestor.php
+++ models/Gestor.php
@@ -1,14 +1,3 @@
 <?php
 class Gestor implements iGestor{
-    public function obtenerTodos(){
-
-    }
-    public function guardar()
-    public function eliminar($id){
-            if ($e->getId() == $id) {
-                unset($_SESSION['Entidad'][$i]);
-                $_SESSION['Entidad'] = array_values($_SESSION['Entidad']);
-                return true;
-            }
-    }
 }
